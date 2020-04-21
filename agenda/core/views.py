aea75a3ef2eab666e760e7ce21b649a80a795a2c
Walison Filipe <walisonfilipe@hotmail.com>
2020-04-21 18:36:20 -0300

    Adiciona filtro na listagem de especialidades

--- agenda/core/views.py
+++ agenda/core/views.py
@@ -1,14 +1,16 @@
-from rest_framework.generics import ListAPIView
+from rest_framework import generics, filters
 
 from .models import Medico, Especialidade
 from .serializers import MedicoSerializer, EspecialidadeSerializer
 
 
-class EspecialidadeList(ListAPIView):
+class EspecialidadeList(generics.ListAPIView):
     queryset = Especialidade.objects.all()
     serializer_class = EspecialidadeSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nome']
 
 
-class MedicoList(ListAPIView):
+class MedicoList(generics.ListAPIView):
     queryset = Medico.objects.all()
     serializer_class = MedicoSerializer
