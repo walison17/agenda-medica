2af86a2f9a263a4856faafd44e18d574f3efac1f
Walison Filipe <walisonfilipe@hotmail.com>
2020-04-22 09:47:13 -0300

    Adiciona filtros por medico e especialidade na listagem de agendas

--- agenda/core/filters.py
+++ agenda/core/filters.py
@@ -16,9 +16,26 @@ class MedicoFilter(django_filters.FilterSet):
 
 
 class AgendaFilter(django_filters.FilterSet):
+    medico = django_filters.ModelMultipleChoiceFilter(
+        queryset=Medico.objects.all(),
+        field_name='medico',
+        label='médico',
+        widget=widgets.QueryArrayWidget()
+    )
+    especialidade = django_filters.ModelMultipleChoiceFilter(
+        queryset=Especialidade.objects.all(),
+        field_name='medico__especialidade',
+        label='especialidade',
+        widget=widgets.QueryArrayWidget()
+    )
     data_inicio = django_filters.DateFilter(field_name='dia', lookup_expr='gte')
     data_final = django_filters.DateFilter(field_name='dia', lookup_expr='lte')
 
     class Meta:
         model = Agenda
-        fields = ['data_inicio', 'data_final']
+        fields = [
+            'medico',
+            'especialidade',
+            'data_inicio',
+            'data_final'
+        ]
