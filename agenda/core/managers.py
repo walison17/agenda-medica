50adf1fe83d111c197844fd5053697a202d527e5
Walison Filipe <walisonfilipe@hotmail.com>
2020-04-22 20:35:09 -0300

    Remove horários passados no queryset da agenda

--- agenda/core/managers.py
+++ agenda/core/managers.py
@@ -1,8 +1,8 @@
-from datetime import date
+from datetime import date, datetime
 
 from django.db import models
 from django.db.models import (
-    Prefetch, OuterRef, Subquery, Exists,
+    Prefetch, OuterRef, Exists, Case, When, Value, Q
 )
 
 
@@ -10,10 +10,21 @@ class AgendaQuerySet(models.QuerySet):
     def prefetch_horarios_disponiveis(self):
         from .models import AgendaHora
 
+        hoje = date.today()
+        hora = datetime.now().strftime('%H:%M')
+
         horarios_disponiveis = (
             AgendaHora
             .objects
-            .filter(disponivel=True)
+            .filter(
+                disponivel=True,
+                hora__gte=Case(
+                    When(
+                        Q(agenda__dia=hoje), then=Value(hora)
+                    ),
+                    default=Value('00:00')
+                )
+            )
         )
 
         return self.prefetch_related(
@@ -25,10 +36,23 @@ class AgendaDisponivelManager(models.Manager):
     def get_queryset(self):
         from .models import AgendaHora
 
-        horarios = (
+        hoje = date.today()
+        hora = datetime.now().strftime('%H:%M')
+
+        horarios_disponiveis = (
             AgendaHora
             .objects
-            .filter(agenda=OuterRef('pk'), disponivel=True)
+            .filter(
+                agenda=OuterRef('pk'),
+                disponivel=True,
+                hora__gte=Case(
+                    When(
+                        Q(agenda__dia=hoje), then=Value(hora)
+                    ),
+                    default=Value('00:00')
+                )
+            )
         )
 
-        return super().get_queryset().filter(dia__gte=date.today()).filter(Exists(horarios))
+        qs = super().get_queryset()
+        return qs.filter(dia__gte=hoje).filter(Exists(horarios_disponiveis))
