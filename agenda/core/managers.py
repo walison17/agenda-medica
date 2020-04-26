bfeecb39c03bd06079499d670f1ad2e65f2a266e
Walison Filipe <walisonfilipe@hotmail.com>
2020-04-26 18:50:49 -0300

    Usa timezone utils

--- agenda/core/managers.py
+++ agenda/core/managers.py
@@ -1,6 +1,5 @@
-from datetime import date, datetime
-
 from django.db import models
+from django.utils import timezone
 from django.db.models import (
     Prefetch, OuterRef, Exists, Case, When, Value, Q
 )
@@ -10,8 +9,8 @@ class AgendaQuerySet(models.QuerySet):
     def carregar_apenas_horarios_disponiveis(self):
         from .models import AgendaHora
 
-        hoje = date.today()
-        hora = datetime.now().strftime('%H:%M')
+        hoje = timezone.localdate()
+        hora = timezone.now().strftime('%H:%M')
 
         horarios_disponiveis = (
             AgendaHora
@@ -36,8 +35,8 @@ class AgendaDisponivelManager(models.Manager):
     def get_queryset(self):
         from .models import AgendaHora
 
-        hoje = date.today()
-        hora = datetime.now().strftime('%H:%M')
+        hoje = timezone.localdate()
+        hora = timezone.now().strftime('%H:%M')
 
         horarios_disponiveis = (
             AgendaHora
