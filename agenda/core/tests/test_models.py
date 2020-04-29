a493517de82a0fe697fda8fadd95efd4e505a630
Walison Filipe <walisonfilipe@hotmail.com>
2020-04-29 11:32:46 -0300

    Testes

--- agenda/core/tests/test_models.py
+++ agenda/core/tests/test_models.py
@@ -3,56 +3,98 @@ from unittest import mock
 
 import pytest
 from model_bakery import baker
-from pytest_django.asserts import assertNumQueries
+from pytest_django.asserts import assertNumQueries, assertQuerysetEqual
 
 from ..models import Agenda
 
 
 @pytest.mark.django_db
 class TestAgendaDisponivelManager:
+    def test_nao_deve_retornar_agendas_sem_horarios_cadastrados(self):
+        baker.make(Agenda, horarios=[])
+
+        assert Agenda.disponiveis.count() == 0
+
     def test_nao_deve_retornar_agendas_de_datas_passadas(self):
-        baker.make(Agenda, dia=date(2020, 11, 14), horarios__disponivel=True, _quantity=4)
-        baker.make(Agenda, dia=date(2020, 11, 16), horarios__disponivel=True, _quantity=2)
+        agenda1 = baker.make(Agenda, dia=date(2020, 11, 14))
+        baker.make('core.AgendaHora', agenda=agenda1)
 
-        with mock.patch('agenda.core.managers.timezone.now', return_value=datetime(2020, 11, 15)):
-            assert Agenda.disponiveis.count() == 2
-            assert Agenda.objects.count() == 6
+        agenda2 = baker.make(Agenda, dia=date(2020, 11, 16))
+        baker.make('core.AgendaHora', agenda=agenda2)
 
-    def test_nao_deve_retornar_horarios_passados(self):
-        baker.make(
+        with mock.patch(
+            'agenda.core.managers.timezone.now',
+            return_value=datetime(2020, 11, 15)
+        ):
+            qs = Agenda.disponiveis.all()
+            assertQuerysetEqual(qs, [agenda2.dia], lambda a: a.dia)
+
+    def test_nao_deve_considerar_horarios_passados_como_disponiveis(self):
+        agenda1 = baker.make(
             Agenda,
             dia=date(2020, 11, 15),
-            horarios__disponivel=True,
-            horarios__hora='09:00'
         )
-        baker.make(
+        baker.make('core.AgendaHora', agenda=agenda1, hora='8:00')
+        baker.make('core.AgendaHora', agenda=agenda1, hora='9:00')
+
+        agenda2 = baker.make(
             Agenda,
             dia=date(2020, 11, 15),
-            horarios__disponivel=True,
-            horarios__hora='11:00'
         )
+        baker.make('core.AgendaHora', agenda=agenda2, hora='10:30')
+        baker.make('core.AgendaHora', agenda=agenda2, hora='11:00')
 
-        with mock.patch('agenda.core.managers.timezone.now', return_value=datetime(2020, 11, 15, hour=10, minute=0)):
-            assert Agenda.disponiveis.count() == 1
-            assert Agenda.objects.count() == 2
+        with mock.patch(
+            'agenda.core.managers.timezone.now',
+            return_value=datetime(2020, 11, 15, 10)  # 15/11/2020 às 10h
+        ):
+            qs = Agenda.disponiveis.all()
+            assertQuerysetEqual(qs, [agenda2.dia], lambda a: a.dia)
 
-    def test_deve_retornar_apenas_agendas_que_possuam_pelo_menos_um_horario_disponivel(self):
-        baker.make(Agenda, dia=date(2020, 11, 16), horarios__disponivel=False, _quantity=2)
-        baker.make(Agenda, dia=date(2020, 11, 17), horarios__disponivel=True, _quantity=1)
-        baker.make(Agenda, dia=date(2020, 11, 18), horarios__disponivel=False, _quantity=1)
+    def test_deve_retornar_apenas_agendas_com_horarios_disponiveis(self):
+        """
+        Deve retornar apenas agendas que possuam horários disponíveis,
+        desconsiderando horários marcados e passados.
+        """
+        dia = date(2020, 11, 15)
 
-        with mock.patch('agenda.core.managers.timezone.now', return_value=datetime(2020, 11, 15)):
-            assert Agenda.disponiveis.count() == 1
-            assert Agenda.objects.count() == 4
+        agenda1 = baker.make(Agenda, dia=dia)
+        baker.make('core.AgendaHora', agenda=agenda1, hora='9:00')
+
+        agenda2 = baker.make(Agenda, dia=dia)
+        baker.make('core.AgendaHora', agenda=agenda2, hora='11:00', disponivel=False)
+
+        agenda3 = baker.make(Agenda, dia=dia)
+        baker.make('core.AgendaHora', agenda=agenda3, hora='9:00')
+        baker.make('core.AgendaHora', agenda=agenda3, hora='11:00')
+        baker.make('core.AgendaHora', agenda=agenda3, hora='11:30', disponivel=False)
+
+        with mock.patch(
+            'agenda.core.managers.timezone.now',
+            return_value=datetime(2020, 11, 15, 10)  # 15/11/2020 às 10h
+        ):
+            qs = Agenda.disponiveis.all()
+            assertQuerysetEqual(qs, [agenda3.pk], lambda a: a.pk)
 
     def test_carregar_apenas_horarios_disponiveis(self):
-        agenda = baker.make(Agenda, dia=date(2020, 11, 17))
+        """
+        Deve fazer o prefetch apenas dos horários disponíveis a atribuir
+        à um atributo chamado `horarios_disponiveis`
+        """
+        agenda = baker.make(Agenda, dia=date(2020, 11, 15))
+        baker.make('core.AgendaHora', agenda=agenda, hora='9:00')
+        baker.make('core.AgendaHora', agenda=agenda, hora='10:30')
+        baker.make('core.AgendaHora', agenda=agenda, hora='11:30', disponivel=False)
 
-        baker.make('core.AgendaHora', agenda=agenda, hora='08:00')
-        baker.make('core.AgendaHora', agenda=agenda, hora='08:30')
-        baker.make('core.AgendaHora', agenda=agenda, hora='09:00', disponivel=False)
+        with mock.patch(
+            'agenda.core.managers.timezone.now',
+            return_value=datetime(2020, 11, 15, 10)  # 15/11/2020 às 10h
+        ):
+            agendas = Agenda.disponiveis.carregar_apenas_horarios_disponiveis()
 
-        with assertNumQueries(2):
-            agenda = Agenda.disponiveis.carregar_apenas_horarios_disponiveis()
+            agenda = agendas[0]
+            assert hasattr(agenda, 'horarios_disponiveis')
 
-            assert len(agenda[0].horarios_disponiveis) == 2
+            assertQuerysetEqual(
+                agenda.horarios_disponiveis, ['10:30'], lambda h: h.hora.strftime('%H:%M')
+            )
